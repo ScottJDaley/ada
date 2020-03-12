c59cde79b51fef6123c6036541adf413b01f019c
Scott <scottjdaley@gmail.com>
2020-03-12 22:15:44 +0000

    Added !recipes "for" and "using"

--- satisfaction.py
+++ satisfaction.py
@@ -23,26 +23,31 @@ class Satisfaction:
     def recipes(self, *args):
         print("calling !recipes with", len(args), "arguments:", ', '.join(args))
 
+        out = []
         if len(args) == 0:
-            out = []
             for recipe in sorted(self.__db.recipes()):
                 out.append(recipe)
-            return '\n'.join(out)
-        if len(args) == 1:
+        elif len(args) == 1:
             arg = args[0]
             if arg not in self.__db.items() and arg not in self.__db.recipes():
                 return "Unknown recipe or item: " + arg
             if arg in self.__db.recipes():
                 return self.__db.recipes()[arg].details()
             if arg in self.__db.items():
-                out = []
-                out.append("Recipes producing item:")
                 for recipe in self.__db.recipes_for_product(arg):
                     out.append(recipe.details())
-                out.append("Recipes requiring item:")
-                for recipe in self.__db.recipes_for_ingredient(arg):
+        elif len(args) == 2:
+            if args[0] != "using" and args[0] != "for":
+                return "Input must be in the form \"!recipes\" \"for\" | \"using\" <item>"
+            if args[1] not in self.__db.items():
+                 return "Unknown item: " + args[1]
+            if args[0] == "using":
+                for recipe in self.__db.recipes_for_ingredient(args[1]):
                     out.append(recipe.details())
-                return '\n'.join(out)
+            elif args[0] == "for":
+                for recipe in self.__db.recipes_for_product(args[1]):
+                    out.append(recipe.details())
+        return '\n'.join(out)
 
     def min(self, *args):
         print("calling !min with", len(args), "arguments:", ', '.join(args))
