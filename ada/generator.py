b9ff3cfa62caf709c45bf97de8b12872473bafdb
AllRead <amorcage@live.fr>
2021-08-16 15:55:01 +0200

    Update generator.py
    update link wiki

--- ada/generator.py
+++ ada/generator.py
@@ -48,7 +48,7 @@ class Generator:
         return '\n'.join(out)
 
     def wiki(self):
-        return "https://satisfactory.gamepedia.com/" + self.human_readable_underscored()
+        return "https://satisfactory.fandom.com/wiki/" + self.human_readable_underscored()
 
     def thumb(self):
         print(ada.image_fetcher.fetch_first_on_page(self.wiki()))
