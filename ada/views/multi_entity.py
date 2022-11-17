dcc2a5c0190d7455fa787d3114532bc83ad5554e
Scott <scottjdaley@gmail.com>
2022-11-17 09:17:10 -0800

    Cleaning up some comments

--- ada/views/multi_entity.py
+++ ada/views/multi_entity.py
@@ -8,29 +8,18 @@ from ada.processor import Processor
 from ada.views.with_previous import WithPreviousView
 
 
-# Defines a custom Select containing colour options
-# that the user can choose. The callback function
-# of this class is called when the user changes their choice
+# See https://github.com/Rapptz/discord.py/blob/master/examples/views/dropdown.py
 class EntityDropdown(discord.ui.Select):
     def __init__(self, vars_: List[Item], processor: Processor):
         self.__processor = processor
-        # Set the options that will be presented inside the dropdown
+
         options = []
         for var in vars_:
             options.append(discord.SelectOption(label=var.human_readable_name(), description=var.var()))
 
-        # The placeholder is what will be shown when no option is chosen
-        # The min and max values indicate we can only pick one of the three options
-        # The options parameter defines the dropdown options. We defined this above
-        super().__init__(placeholder=f'Found {len(vars_)} matches.', min_values=1, max_values=1, options=options)
+        super().__init__(placeholder=f'Found {len(vars_)} matches', min_values=1, max_values=1, options=options)
 
     async def callback(self, interaction: discord.Interaction):
-        # Use the interaction object to send a response message containing
-        # the user's favourite colour or choice. The self object refers to the
-        # Select object, and the values attribute gets a list of the user's
-        # selected options. We only want the first one.
-        # await interaction.response.send_message(f'Your favourite colour is {self.values[0]}')
-
         breadcrumbs = Breadcrumbs.extract(interaction.message.content)
         query = f"{self.values[0]}"
         breadcrumbs.add_query(query)
