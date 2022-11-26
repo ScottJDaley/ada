323f09dafc6c54051c4aa0b6de43693e3614ae41
Scott <scottjdaley@gmail.com>
2022-11-26 10:20:19 -0800

    Add try except around image fetcher

--- ada/utils/image_fetcher.py
+++ ada/utils/image_fetcher.py
@@ -5,12 +5,13 @@ from bs4 import BeautifulSoup
 
 
 def fetch_first_on_page(url: str) -> None | str:
-    html = urlopen(url)
-    bs = BeautifulSoup(html, "html.parser")
-    image_element = bs.find(
-        "img", {"src": re.compile(".png"), "class": "pi-image-thumbnail"}
-    )
-    if image_element is None:
+    try:
+        html = urlopen(url)
+        bs = BeautifulSoup(html, "html.parser")
+        image_element = bs.find(
+            "img", {"src": re.compile(".png"), "class": "pi-image-thumbnail"}
+        )
+    except:
         print(
             "Could not find any png image with class name 'pi-image-thumbnail' on the following page:",
             url,
