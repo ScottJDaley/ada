8c63101df8113dd1efb71ddbfcdd6cb019299704
Scott <scottjdaley@gmail.com>
2023-06-19 20:38:44 -0700

    Update how images are retrieved from new wiki

--- ada/utils/image_fetcher.py
+++ ada/utils/image_fetcher.py
@@ -1,12 +1,13 @@
 import re
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 
 from bs4 import BeautifulSoup
 
 
 def fetch_first_on_page(url: str) -> None | str:
     try:
-        html = urlopen(url)
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req)
         bs = BeautifulSoup(html, "html.parser")
         image_element = bs.find(
             "img", {"src": re.compile(".png"), "class": "pi-image-thumbnail"}
@@ -17,4 +18,4 @@ def fetch_first_on_page(url: str) -> None | str:
             url,
         )
         return None
-    return image_element["src"]
+    return "https://satisfactory.wiki.gg" + image_element["src"]
